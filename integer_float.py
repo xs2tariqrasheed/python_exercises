e1d35efd1bb1595954ad6ac38d9595d5da31318c
tariq-rasheed <tariq.rasheed@peeredge.io>
2020-04-02 09:24:36 +0500

    arithmetic progression added

--- integer_float.py
+++ integer_float.py
@@ -1,7 +1,17 @@
 _ = print
 
-c = 4 + 2j  # complex
-print(type(c))
+# NOTE: don't compare floating number with ==
+# A better way to compare floating point numbers is to assume that two
+# numbers are equal if the difference between them is less than ε , where ε is a
+# small number.
+# In practice, the numbers can be compared as follows ( ε = 10 − 9 )
+# if abs(a - b) < 1e-9: # a and b are equal
+
+# _(0.3*3+0.1)
+# x = 4.66668
+# _(round(x, 2))
+# c = 4 + 2j  # complex
+# print(type(c))
 # i = 9999999999999999999999999999999999
 # f = 0.00000000000000000001
 # print(f)
